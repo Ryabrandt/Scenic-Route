c2bcd647420931891e9468c252a537252b2d91c2
Ryan Brandt <Ryabrandt@gmail.com>
2014-02-03 08:56:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scenicroute::Application.config.secret_key_base = '438083b5a78c03928cea1053ba26c15210baa3a03507db437a52caa97466dbe29fa2ef6ff0073c4b277cc6da395141c41aa97bfa60b6a8fd18fb062138005cd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scenicroute::Application.config.session_store :cookie_store, key: '_scenicroute_session'
