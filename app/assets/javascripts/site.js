d64a8ee19792ac47deff3b1873cea0209b65de9d
Ryan Brandt <Ryabrandt@gmail.com>
2014-02-06 15:33:42 -0800

    fancy styling

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,3 +1,51 @@
+$(document).ready(function () {
+    //stick in the fixed 100% height behind the navbar but don't wrap it
+    $('#slide-nav.navbar .container').append($('<div id="navbar-height-col"></div>'));
+    // Enter your ids or classes
+    var toggler = '.navbar-toggle';
+    var pagewrapper = '#page-content';
+    var navigationwrapper = '.navbar-header';
+    var menuwidth = '100%'; // the menu inside the slide menu itself
+    var slidewidth = '80%';
+    var menuneg = '-100%';
+    var slideneg = '-80%';
+
+
+    $("#slide-nav").on("click", toggler, function (e) {
+
+        var selected = $(this).hasClass('slide-active');
+
+        $('#slidemenu').stop().animate({
+            left: selected ? menuneg : '0px'
+        });
+
+        $('#navbar-height-col').stop().animate({
+            left: selected ? slideneg : '0px'
+        });
+
+        $(pagewrapper).stop().animate({
+            left: selected ? '0px' : slidewidth
+        });
+
+        $(navigationwrapper).stop().animate({
+            left: selected ? '0px' : slidewidth
+        });
+
+
+        $(this).toggleClass('slide-active', !selected);
+        $('#slidemenu').toggleClass('slide-active');
+
+
+        $('#page-content, .navbar, body, .navbar-header').toggleClass('slide-active');
+    });
+    var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+    $(window).on("resize", function () {
+        if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+            $(selected).removeClass('slide-active');
+        }
+    });
+});
+
 $(function(){
     $("#show_new_route").click(codeAddress);
     $("#save_new_route").click(function(){
@@ -29,6 +77,7 @@ var directionsService = new google.maps.DirectionsService();
 var map;
 var sf = new google.maps.LatLng(37.7434701,-122.4557752);
 var geocoder = new google.maps.Geocoder();
+
 
 function initialize() {
 
@@ -92,9 +141,9 @@ function codeAddress(event) {
 
 }
 
-
-
+if ($("#map_canvas") && $("#directionsPanel") !== null){
  google.maps.event.addDomListener(window, 'load', initialize);
+}
 
 
 function calcSavedRoute() {
