d886122b87fceff889e6cd901bec6becbab587c5
Ryan Brandt <Ryabrandt@gmail.com>
2014-02-12 08:52:51 -0800

    testing to see if spec did not get pushed first time

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -2,29 +2,53 @@ require 'spec_helper'
 
 describe TripsController do
 
+	describe "GET root" do
+		it "should redirect to the trip index" do
+			pending
+			# how to have it send to the root from rspec?
+		end
+	end
+
 	describe "GET index" do
+	  before(:each) do
+	  	@trip = mock_model("Trip")
+	  	allow(Trip).to receive(:new).and_return(@trip)
+	  	allow(Trip).to receive(:all).and_return(@trip)
+	  end
+
 		it "returns http success" do
 			get "index"
 			expect(response).to be_success
 		end
-	end
-
-
-
-
-
 	
+		it "displays all trips on visiting" do
+			allow(Trip).to receive(:all)
+		end
 
-	describe "GET new" do
+
 	end
+
+
+
+
+
 
 	describe "POST create" do
+		before(:each) do
+  		todo = mock_model("Trip")
+  		allow(Trip).to receive(:create)
+	end
+
 	end
 
 	describe "GET show" do
 	end
 
 	describe "GET about" do
+		it "should render the about view" do
+			get :about
+			expect(response).to render_template(:about)
+		end
 	end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -25,6 +26,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.include Rails.application.routes.url_helpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
